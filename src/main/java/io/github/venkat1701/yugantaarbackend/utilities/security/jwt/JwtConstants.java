8b8d1a2d17b79402250a768be19438de99be3ad7
Krish Jaiswal <87254400+venkat1701@users.noreply.github.com>
2024-11-05 04:11:36 +0530

    Educational SECRET_KEY

--- src/main/java/io/github/venkat1701/yugantaarbackend/utilities/security/jwt/JwtConstants.java
+++ src/main/java/io/github/venkat1701/yugantaarbackend/utilities/security/jwt/JwtConstants.java
@@ -1,7 +1,7 @@
 package io.github.venkat1701.yugantaarbackend.utilities.security.jwt;
 
 public class JwtConstants {
-
+    // Just an example SECRET_KEY. Kindly generate yours or use this. This is used for testing purposes only and is not deployed, so happy hacking:!
     public static final String SECRET_KEY = "rRHjbQ1kgvZg970iZw9phjJ5YXDpAjaNw9Vy+TTMXLY=";
     public static final String TOKEN_PREFIX = "Bearer ";
 }
